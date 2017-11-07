687690a584ab2d3178cb1d83e4533a976a047e1b
Francis Yang <xyang42@ucsc.edu>
2017-11-06 19:17:15 -0800

    Added DP method for roll-dice problem

--- Tests/Backtracking/SolutionTest.java
+++ Tests/Backtracking/SolutionTest.java
@@ -10,6 +10,18 @@ class SolutionTest {
 	}
 	@Test
 	void combinationSum() {
-		System.out.print(solution.combinationSum(9));
+		System.out.print(solution.combinationSum(4));
+	}
+	@Test
+	void combinationSumDP() {
+		System.out.print(solution.combinationSumDP(599));
+	}
+	@Test
+	void combinationSumDTM() {
+		System.out.print(solution.combinationSumDTM(3));
+	}
+	@Test
+	void combinationSumDP2() {
+		System.out.print(solution.combinationSumDP2(9));
 	}
 }
--- src/Backtracking/Solution.java
+++ src/Backtracking/Solution.java
@@ -3,6 +3,7 @@ package Backtracking;
 public class Solution {
 
 	int result = 0;
+	long dp[] = new long[611];
 //Problem: Imagine you are playing a board game. You roll a 6-faced dice
 //and move forward the same number of spaces that you rolled.
 //If the finishing point is “n” spaces away from the starting point,
@@ -25,5 +26,52 @@ public class Solution {
 				backtrack(nums, remain - nums[i], i);
 			}
 		}
+	}
+
+	public long combinationSumDP (int target) {
+		for (int i = 1; i <= target; i++) {
+			long tmp = 0;
+
+			for (int j = 1; j <= 6; j++) {
+				if (i == j)
+					tmp++;
+				if (i > j)
+					tmp += dp[i - j];
+			}
+			dp[i] = tmp;
+		}
+		return dp[target];
+	}
+
+	public int combinationSumDTM (int target) {
+		int[] nums = {1, 2, 3, 4, 5, 6};
+		return backtrack_new(nums, target, 5);
+	}
+
+	private int backtrack_new (int[] nums, int remain, int kinds) {
+		if (remain < 0)	return 0;
+		if (remain == 0) return 1;
+		if (kinds < 0) return 0;
+		else return backtrack_new(nums, remain, kinds - 1) + backtrack_new(nums, remain - nums[kinds], kinds);
+	}
+
+	public long combinationSumDP2 (int target) {
+		long[] dp = new long[target + 1];
+		return backtrack_DP2(target, dp);
+	}
+
+	private long backtrack_DP2 (int remain, long dp[]) {
+		if (remain == 0) {
+			return 0;
+		}
+		if (dp[remain] == 0) {
+			for (int j = 1; j <= 6; j++) {
+				if (remain == j)
+					dp[remain]++;
+				else if (remain > j)
+					dp[remain] += backtrack_DP2(remain - j, dp);
+			}
+		}
+		return dp[remain];
 	}
 }
