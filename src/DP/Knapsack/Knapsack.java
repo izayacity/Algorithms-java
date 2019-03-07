b3153151f339e93725448700e45b8b89d650a860
Francis Yang <izayacity@gmail.com>
2019-03-07 22:59:29 +0800

    0-1 knapsack

--- src/DP/Knapsack/Knapsack.java
+++ src/DP/Knapsack/Knapsack.java
@@ -1,23 +1,30 @@
 package DP.Knapsack;
 
+import java.util.Arrays;
+
 // https://www.geeksforgeeks.org/knapsack-problem/
 public class Knapsack {
 	// O(nW) where n is the number of items and W is the capacity of knapsack.
 	int knapSack(int W, int wt[], int val[], int n) {
 		int dp[][] = new int[n + 1][W + 1];
-
-		for(int i = 0; i <= n; i++) {
-			for(int w = 0; w <= W; w++) {
-				if(i == 0 || w == 0) {
-					dp[i][w] = 0;
-				} else if (wt[i - 1] > w) {
-					dp[i][w] = dp[i - 1][w];		// i th item not included
-				} else {
-					dp[i][w] = Math.max(dp[i - 1][w], val[i - 1] + dp[i - 1][w - wt[i - 1]]);
-				}
-			}
+		for (int[] arr : dp) {
+			Arrays.fill(arr, -1);
 		}
-		return dp[n][W];
+		return knapSackUtil(W, wt, val, n, dp);
+	}
+
+	int knapSackUtil(int W, int wt[], int val[], int n, int[][] dp) {
+		if(n == 0 || W == 0) {
+			return 0;
+		}
+		if (dp[n][W] != -1) {
+			return dp[n][W];
+		}
+		if(wt[n - 1] > W) {
+			return knapSackUtil(W, wt, val, n - 1, dp);
+		}
+		return dp[n][W] = Math.max(val[n - 1] + knapSackUtil(W - wt[n - 1], wt, val, n - 1, dp),
+				knapSackUtil(W, wt, val, n - 1, dp));
 	}
 
 	// Time complexity of this naive recursive solution is exponential (2^n).
