0e955a1c9811c128b10ae7453b13fae2eab1e758
izayacity <izayacity@gmail.com>
2019-03-14 13:24:45 +0800

    roll dice dp problem

--- Tests/Backtracking/SolutionTest.java
+++ Tests/Backtracking/SolutionTest.java
@@ -1,6 +1,6 @@
 package Backtracking;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 class SolutionTest {
 	private Solution solution;
@@ -9,19 +9,7 @@ class SolutionTest {
 		solution = new Solution();
 	}
 	@Test
-	void combinationSum() {
-		System.out.print(solution.combinationSum(4));
-	}
-	@Test
-	void combinationSumDP() {
-		System.out.print(solution.combinationSumDP(599));
-	}
-	@Test
-	void combinationSumDTM() {
-		System.out.print(solution.combinationSumDTM(3));
-	}
-	@Test
-	void combinationSumDP2() {
-		System.out.print(solution.combinationSumDP2(9));
+	void rollDice() {
+		System.out.print(solution.rollDice(4));
 	}
 }
